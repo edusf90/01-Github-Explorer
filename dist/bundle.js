b3c9a4834040e8b955e368ca8138717e618cf268
Eduardo Santana <edusf0@hotmail.com>
2021-05-03 23:14:34 -0300

    first commit

---
+++ dist/bundle.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// const user = {
+//     name: 'Eduardo'
+// }
+// console.log(user.address?.street)
+function app() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+// const user = {
+//     name: 'Eduardo'
+// }
+
+// console.log(user.address?.street)
+
+import React from 'react';
+
+function app() {
+    return <h1>Hello World</h1>
+}
