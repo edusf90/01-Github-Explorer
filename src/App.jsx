baaddf286a411a962a6c56d43f543d9abe0975ef
Eduardo Santana <edusf0@hotmail.com>
2021-05-10 21:23:41 -0300

    Estilização da listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { Counter } from './styles/components/Counter'
-import { RepositoryItem } from './styles/components/RepositoryItem'
-import { RepositoryList } from './styles/components/RepositoryList'
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
@@ -8,7 +6,6 @@ export function App() {
     return (
         <>
             <RepositoryList />
-            <Counter />
         </>
     )
 }
